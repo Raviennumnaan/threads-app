39ce214b23a46b004a2645db5a10716d190209e0
Ravishankar M <raviennumnaan@gmail.com>
2024-04-11 02:51:04 +0530

    first commit

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   publicRoutes: ["/", "/api/webhook/clerk"],
-  ignoredRoutes: ["/api/webhook/clerk"],
+  ignoredRoutes: ["/api/webhook/clerk", "/profile/null"],
 });
 export const config = {
   matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
